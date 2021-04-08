c86b06425598dfa5ab01368c2b9c133acc50afc7
ngetichnick <nicholas.ngetich@student.moringaschool.com>
2021-04-08 12:02:27 +0300

    Add function and a button for subtraction operation

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
+  
 var addition =() => {
   var add = function(number1, number2) {
     return number1 + number2;
   };
-  
   var number1 = parseInt(prompt("Enter a number:"));
   var number2 = parseInt(prompt("Enter another number:"));
   var result = add(number1, number2);
+  alert(result);
+}
+var subtraction = () => {
+  var minus = function(number1, number2) {
+    return number1 - number2;
+  };
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  var result = minus(number1, number2);
   alert(result);
 }
