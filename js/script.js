9d62f6addc09677688db3c2b2d02a2a3b9d52dca
ngetichnick <nicholas.ngetich@student.moringaschool.com>
2021-04-17 19:28:57 +0300

    Update calculator

--- js/script.js
+++ js/script.js
@@ -14,8 +14,12 @@ var divide = function(number1, number2) {
 
 //user interface(front3-end)logic
 $(document).ready(function() {
-  var number1 = parseInt(prompt("Enter a number:"));
-  var number2 = parseInt(prompt("Enter another number"));
-  alert(add(number1,number2));
-})
+  $("form#add").submit(function(event) {
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1,number2);
+    $("#output").text(result);
+    event.preventDefault();
+  });
+});
 
