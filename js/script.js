49ca0016f64e7cd6f9a63c29ea90ac517ca948d9
ngetichnick <nicholas.ngetich@student.moringaschool.com>
2021-04-17 18:58:13 +0300

    Add business and user interface logic

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
-  
-var addition =() => {
-  var add = function(number1, number2) {
-    return number1 + number2;
-  };
+//Business(back-end)logic
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+//user interface(front3-end)logic
+$(document).ready(function() {
   var number1 = parseInt(prompt("Enter a number:"));
-  var number2 = parseInt(prompt("Enter another number:"));
-  var result = add(number1, number2);
-  alert(result);
-}
-var subtraction = () => {
-  var minus = function(number1, number2) {
-    return number1 - number2;
-  };
-  var number1 = parseInt(prompt("Enter a number:"));
-  var number2 = parseInt(prompt("Enter another number:"));
-  var result = minus(number1, number2);
-  alert(result);
-}
+  var number2 = parseInt(prompt("Enter another number"));
+  alert(add(number1,number2));
+})
+
