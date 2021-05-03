a613b5c2e2d93c703f49a43289668386ea0bae0b
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-05-03 18:22:45 +0300

    Update operators

--- js/script.js
+++ js/script.js
@@ -14,12 +14,34 @@ var divide = function(number1, number2) {
 
 //user interface(front3-end)logic
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
+  $("#add").click(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#number1").val());
+    var number2 = parseInt($("#number2").val());
     var result = add(number1,number2);
     $("#output").text(result);
+  });
+  $("#minus").click(function(event) {
     event.preventDefault();
+    var number1 = parseInt($("#number1").val());
+    var number2 = parseInt($("#number2").val());
+    var result = subtract(number1,number2);
+    $("#output").text(result);
+  });
+  $("#multiply").click(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#number1").val());
+    var number2 = parseInt($("#number2").val());
+    var result = multiply(number1,number2);
+    $("#output").text(result);
+  });
+  $("#divide").click(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#number1").val());
+    var number2 = parseInt($("#number2").val());
+    var result = divide(number1,number2);
+    $("#output").text(result);
   });
 });
+
 
